cbb23e20a1657091622c4d2fae1290b0c21b1dd4
Melvin <melvin@melvinkool.nl>
2021-09-21 13:11:04 +0200

    Fixed split function for negative numbers, added num_bits function

--- assignment1/common.py
+++ assignment1/common.py
@@ -1,6 +1,12 @@
 # Splits the string str into a list of integers with base b.
-def split(str, b):
-    return [int(i, b) for i in list(str)]
+def split(i_str, b):
+    if b > 16:
+        raise ValueError('Base > 16 not supported')
+    is_negative = i_str.lstrip()[0] == '-'
+    allowed_chars = '0123456789'
+    allowed_chars += 'abcdef'[:max(b - 10,0)]
+    digits = [int(c,b) for c in i_str if c in allowed_chars]
+    return [1 if is_negative else 0] + digits
 
 # Maps the integer n to a string that goes until the hexadecimals.
 def baseToString(n):
@@ -9,6 +15,9 @@ def baseToString(n):
 # Concatinates the array arr of integers into a string using the baseToString function.
 def concat(arr):
     return ''.join([baseToString(i) for i in arr])
+
+def num_bits(x):
+    return len(x) -  1
 
 # Shifts the array arr by adding n zeros at the end of the arr.
 def radixShift(arr, n):
