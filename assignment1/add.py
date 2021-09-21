3bde6fa49f1813dc26289f45f335ff47e33ad06c
RJ <r.m.jaspers@student.tue.nl>
2021-09-21 13:20:45 +0200

    Added add function and modified script to output test cases

--- assignment1/add.py
+++ assignment1/add.py
@@ -1,2 +1,42 @@
-def add():
-    pass
+import common
+
+
+def add(x, y, b, params):
+    carry = 0
+    z = []
+
+    for i in range(len(x), max(len(x), len(y))):
+            x[i] = 0
+
+    for i in range(len(y), max(len(x), len(y))):
+            y[i] = 0
+
+    for i in range(0, max(len(x), len(y))):
+        z.append(x[i] + y[i] + carry)
+        if z[i] >= b:
+            z[i] = z[i] - b
+            carry = 1
+        else:
+            carry = 0
+
+    if carry == 1:
+        k = max(len(x), len(y)) + 1
+        z.append(1)
+    else:
+        k = max(len(x), len(y))
+
+    return z
+
+
+def calc(params):
+    # Parses the base b from the params
+    b = int(params['radix'])
+    # Parses the x value from the params
+    x = common.split(params['x'], b)
+    # Parses the y value from the params
+    y = common.split(params['y'], b)
+
+    # Calculates and parses the answer
+    params['answer'] = common.concat(add(x, y, b, params))
+
+    return params
--- assignment1/script.py
+++ assignment1/script.py
@@ -17,77 +17,77 @@ import subtract
 # set file names
 base_location = './'
 ops_loc = base_location + 'operations.asn'
-exs_loc = base_location + 'my_exercises'
+exs_loc = base_location + 'test_exercises_students_answers'
 ans_loc = base_location + 'my_answers'
 
-###### Creating an exercise list file ######
-
-# How to create an exercise JSON file containing one addition exercise
-exercises = {'exercises' : []}                                     # initialize empty exercise list
-ex = {'add' : {'radix' : 10, 'x' : '3', 'y' : '4', 'answer' : ''}} # create add exercise
-exercises['exercises'].append(ex)                                  # add exercise to list
-
-# Encode exercise list and print to file
-my_file = open(exs_loc, 'wb+')                                     # write to binary file
-my_file.write(json.dumps(exercises).encode())                      # add encoded exercise list
-my_file.close()
+# ###### Creating an exercise list file ######
+#
+# # How to create an exercise JSON file containing one addition exercise
+# exercises = {'exercises' : []}                                     # initialize empty exercise list
+# ex = {"add": {"radix": 16, "x": "82a4d3f8bfab54bb3011", "y": "cb95aa820d14888e48c3", "answer": ""}} # create add exercise
+# exercises['exercises'].append(ex)                                  # add exercise to list
+#
+# # Encode exercise list and print to file
+# my_file = open(exs_loc, 'wb+')                                     # write to binary file
+# my_file.write(json.dumps(exercises).encode())                      # add encoded exercise list
+# my_file.close()
 
 ###### Using an exercise list file ######
 
 # Compile specification
-spec = asn.compile_files(ops_loc, codec = "jer")
+spec = asn.compile_files(ops_loc, codec="jer")
 
-# Read exercise list 
-exercise_file = open(exs_loc, 'rb')                                # open binary file
-file_data = exercise_file.read()                                   # read byte array
-my_exercises = spec.decode('Exercises', file_data)                 # decode after specification
-exercise_file.close()                                              
+# Read exercise list
+exercise_file = open(exs_loc, 'rb')  # open binary file
+file_data = exercise_file.read()  # read byte array
+my_exercises = spec.decode('Exercises', file_data)  # decode after specification
+exercise_file.close()
 
 # Create answer JSON
 my_answers = {'exercises': []}
 
 # Loop over exercises and solve
 for exercise in my_exercises['exercises']:
-    operation = exercise[0]                                        # get operation type
-    params = exercise[1]                                           # get parameters
-    
+    operation = exercise[0]  # get operation type
+    params = exercise[1]  # get parameters
+
     if operation == 'add':
-        params = add.add();
-    
-    if operation == 'mod-add':
-        params = mod_add.calc(params);
-    
-    if operation == 'subtract':
-        params = subtract.subtract();
+        calc_params = add.calc(params)
 
-    if operation == 'mod-subtract':
-        params = mod_subtract.modSubtract();
-
-    if operation == 'multiply':
-        params = multiply.multiply();
-
-    if operation == 'mod-multiply':
-        params = mod_multiply.modMultiply();
-    
-    if operation == 'karatsuba':
-        params = karatsuba.calc(params);
-    
-    if operation == 'reduce':
-        params = reduce.reduce();
-    
-    if operation == 'euclid':
-        params = euclid.euclid();
-    
-    if operation == 'inverse':
-        params = inverse.inverse();
+    # if operation == 'mod-add':
+    #     params = mod_add.calc(params);
+    #
+    # if operation == 'subtract':
+    #     params = subtract.subtract();
+    #
+    # if operation == 'mod-subtract':
+    #     params = mod_subtract.modSubtract();
+    #
+    # if operation == 'multiply':
+    #     params = multiply.multiply();
+    #
+    # if operation == 'mod-multiply':
+    #     params = mod_multiply.modMultiply();
+    #
+    # if operation == 'karatsuba':
+    #     params = karatsuba.calc(params);
+    #
+    # if operation == 'reduce':
+    #     params = reduce.reduce();
+    #
+    # if operation == 'euclid':
+    #     params = euclid.euclid();
+    #
+    # if operation == 'inverse':
+    #     params = inverse.inverse();
 
     # Save answer
+    print(params['answer'] + "=" + calc_params['answer'] + "?" + str(calc_params['answer'] is params['answer']))
     my_answers['exercises'].append({operation: params})
 
 ###### Creating an answers list file ######
 
 # Save exercises with answers to file
-my_file = open(ans_loc, 'wb+')                                       # write to binary file
-my_file.write(json.dumps(my_answers).encode())                       # add encoded exercise list
+my_file = open(ans_loc, 'wb+')  # write to binary file
+my_file.write(json.dumps(my_answers).encode())  # add encoded exercise list
 my_file.close()
-
