b081dce390ffa9ddd715946a6ea54a2e11367f2f
Jort van Driel <10116429+JortvD@users.noreply.github.com>
2021-09-24 11:01:25 +0200

    Add a failing case to the lt algo

--- assignment1/tests/test_datatypes.py
+++ assignment1/tests/test_datatypes.py
@@ -1,4 +1,6 @@
 import unittest
+import sys
+sys.path.append("..")
 import datatypes
 
 class TestSubtract(unittest.TestCase):
@@ -10,9 +12,9 @@ class TestSubtract(unittest.TestCase):
         self.assertFalse(x < y)
 
         # Test leading zeroes
-        x = datatypes.LargeInteger('06', 10)
-        y = datatypes.LargeInteger('11', 10)
-        self.assertTrue(x < y)
+        x = datatypes.LargeInteger('-6', 10)
+        y = datatypes.LargeInteger('-11', 10)
+        self.assertFalse(x < y)
 
 if __name__ == '__main__':
     unittest.main()
