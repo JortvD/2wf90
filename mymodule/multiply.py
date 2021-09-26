720c3ee4f814786bbfeaa13e1f6e3005bc11e1c8
Chiara <c.liotta@student.tue.nl>
2021-09-26 16:32:53 +0200

    clean up multiply

--- mymodule/multiply.py
+++ mymodule/multiply.py
@@ -2,8 +2,10 @@ import copy
 import math
 from mymodule import datatypes
 
+
 def multiply(x, y):
     sign = 0
+
     if x.is_negative ^ y.is_negative:
         sign = 1
 
@@ -20,20 +22,20 @@ def multiply(x, y):
 
     x_1.invert_digits()
     y_1.invert_digits()
+
     for i in range(0, m):
         carry = 0
         for j in range(0, n):
-            t = z[i+j] + (x_1[i]*y_1[j]) + carry
-            carry = math.floor(t / x_1.radix)
-            z[i+j] = t - (carry*x_1.radix)
+            t = z[i+j] + x_1[i] * y_1[j] + carry
+            carry = t // x_1.radix
+            z[i+j] = t - (carry * x_1.radix)
         z[i+n] = carry
-    if z[m+n-1] == 0:
-        k = m + n - 2
-    else:
-        k = m + n - 1
 
     z.invert_digits()
+
     if sign == 1:
         z.make_negative()
 
-    return datatypes.LargeInteger(str(z), 10)
+    z.strip_leading_zeroes()
+
+    return z
