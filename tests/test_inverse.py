6b5181a82f75e6a019d1312c0dbb4ae96082dda5
RJ <r.m.jaspers@student.tue.nl>
2021-09-26 14:19:32 +0200

    Added inverse based on Euclid with some tests

---
+++ tests/test_inverse.py
@@ -0,0 +1,29 @@
+import unittest
+from mymodule import datatypes
+from mymodule.inverse import inverse
+
+class TestInverse(unittest.TestCase):
+
+    def test_inverse(self):
+        a = datatypes.LargeInteger('11', 10)
+        m = datatypes.LargeInteger('5', 10)
+        res = inverse(a, m)
+        self.assertEqual(str(res), '1')
+
+        a = datatypes.LargeInteger('3', 10)
+        m = datatypes.LargeInteger('5', 10)
+        res = inverse(a, m)
+        self.assertEqual(str(res), '2')
+
+        a = datatypes.LargeInteger('14234', 10)
+        m = datatypes.LargeInteger('13431', 10)
+        res = inverse(a, m)
+        self.assertEqual(str(res), 'inverse does not exist')
+
+        a = datatypes.LargeInteger('3', 10)
+        m = datatypes.LargeInteger('-5', 10)
+        res = inverse(a, m)
+        self.assertEqual(str(res), '2')
+
+if __name__ == '__main__':
+    unittest.main()
